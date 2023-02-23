7cd1c36d2b75923a7ae6c0b77b592dd841ffde10
root <root@DESKTOP-903EI4M.localdomain>
2023-02-23 14:31:21 -0800

    add two_sum func

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
@@ -4,4 +4,16 @@ class Array
         self.each {|ele| hash[ele] = true}
         hash.keys
     end
+
+    def two_sum 
+        result = []
+        self.each_with_index do |num_1, idx_1|
+            self.each_with_index do |num_2, idx_2|
+                if idx_2 > idx_1 && num_1 + num_2 == 0
+                    result << [idx_1, idx_2]
+                end
+            end
+        end
+        return result
+    end
 end
--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
@@ -10,6 +10,16 @@ describe "my_uniq" do
     it "should return array with no duplicates" do 
         expect(arr1.my_uniq).to eq([1,2,3])
     end
+end
 
+describe "two_sum" do
+    subject(:arr1) { [-1,0,2,-2,1] }
+    it "should return a two-dimensional array containing pairs that sum to zero" do
+        expect(arr1.two_sum).to eq([[0, 4], [2, 3]])
+    end
 
+    it "should sort the indices smallest to greatest" do 
+        arr_2 = [-1, 1]
+        expect(arr_2.two_sum[0][0]).to be < (arr_2.two_sum[0][1])
+    end
 end
